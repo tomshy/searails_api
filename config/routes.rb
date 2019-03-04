342a26292681b608e251ff5663d4706a61503cb1
tomshy <tomshy@localhost.localdomain>
2019-03-04 14:11:30 +0300

    deleted unnecessary files and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 	namespace :api do
-		namespace :v1 do
-			resources :users do				
+		namespace :v1 do						
+				resources :users, only: [:create, :index]
 				resources :blogs do
-					resources :articles do
-						resources :comments
-					end
+					resources :articles, only: [:create, :index]
 				end
-			end			
+				resources :articles, except: [:create, :index, :new] do
+					resources :comments, shallow: true
+				end			
 		end	  	
 	end
 end
